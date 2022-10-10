46b7d37817ca0e3077fd49e8035760b17d059669
SlowProgramm <arturzlo13@gmail.com>
2022-10-10 20:50:39 +0300

    Fixed test_set_engine Everything is Done!!

--- homework_02/car.py
+++ homework_02/car.py
@@ -2,19 +2,22 @@ from homework_02.base import Vehicle
 """
 создайте класс `Car`, наследник `Vehicle`
 """
-
+from homework_02.engine import Engine
+#pytest testing/test_homework_02 -s -vv
 class Car(Vehicle):
     engine = None
-    def set_engine(self, Engine):
-        self.engine = str(Engine)
-        print(self.fuel)
-'''
     def __init__(self, weight, fuel, fuel_consumption):
         self.weight = weight
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
+    def set_engine(self, Engine):
+        self.engine = Engine
+        print(self.fuel)
+
+
 '''
+car = Car(0,0,0)
 
-
-
+print(car)
+'''
 
