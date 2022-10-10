944a28df747be16be0e0feed3e8ad36f2dc5d863
SlowProgramm <arturzlo13@gmail.com>
2022-10-10 19:02:41 +0300

    Fixed test_init_plane

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,6 +1,7 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from homework_02.exceptions import CargoOverload
 from homework_02.base import Vehicle
 
 
@@ -8,18 +9,19 @@ class Plane(Vehicle):
     cargo = 0
     max_cargo = 0
 
-    def __init__(self,max_cargo):
+    def __init__(self,weight, fuel, fuel_consumption, max_cargo):
         self.max_cargo = max_cargo
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
 
-    def loadcargo(self, num):
+    def load_cargo(self, num):
         if (self.cargo + num) <= self.max_cargo:
             self.cargo = self.cargo + num
         else:
-            raise Exception("CargoOverload")
+            raise CargoOverload()
 
     def remove_all_cargo(self):
         oldCargo, self.cargo = self.cargo, 0
         return oldCargo
 
-    def __str__(self):
-        return f"cargo = {self.cargo} max_cargo = {self.max_cargo}"
