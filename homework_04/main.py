214da68fef76d11586d4b023f86ec2c6bf1d8206
SlowProgramm <arturzlo13@gmail.com>
2022-12-18 22:00:07 +0300

    First commit (at least works, without tests)

---
+++ homework_04/main.py
@@ -0,0 +1,58 @@
+"""
+Домашнее задание №4
+Асинхронная работа с сетью и бд
+
+доработайте функцию main, по вызову которой будет выполняться полный цикл программы
+(добавьте туда выполнение асинхронной функции async_main):
+- создание таблиц (инициализация)
+- загрузка пользователей и постов
+    - загрузка пользователей и постов должна выполняться конкурентно (параллельно)
+      при помощи asyncio.gather (https://docs.python.org/3/library/asyncio-task.html#running-tasks-concurrently)
+- добавление пользователей и постов в базу данных
+  (используйте полученные из запроса данные, передайте их в функцию для добавления в БД)
+- закрытие соединения с БД
+"""
+
+# Хотел задать вопрос. Что означает ошибка "Event loop is closed", которую игнорирует PyCharm?
+
+import asyncio
+from models import engine, Base, Session, User, Post
+from jsonplaceholder_requests import fetch_json_data, USERS_DATA_URL, POSTS_DATA_URL
+
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def add_all(session, users_data, posts_data):
+    for el in users_data:
+        json_id = el.get('id')
+        name = el.get('name')
+        username = el.get('username')
+        email = el.get('email')
+        new_user = User(json_id=json_id, name=name, username=username, email=email)
+        session.add(new_user)
+        for el2 in posts_data:
+            if new_user.json_id == el2.get('userId'):
+                title = el2.get('title')
+                body = el2.get('body')
+                new_post = Post(body=body, title=title, user=new_user)
+                session.add(new_post)
+    await session.commit()
+
+
+async def async_main():
+    await create_tables()
+    users_data, posts_data = await asyncio.gather(fetch_json_data(USERS_DATA_URL), fetch_json_data(POSTS_DATA_URL))
+    async with Session() as session:
+        await add_all(session, users_data, posts_data)
+
+
+def main():
+    asyncio.run(async_main())
+
+
+if __name__ == "__main__":
+    main()
---
+++ homework_04/models.py
@@ -0,0 +1,64 @@
+"""
+создайте алхимичный engine +
+добавьте declarative base (свяжите с engine) +
+создайте объект Session +
+добавьте модели User и Post, объявите поля: +
+для модели User обязательными являются name, username, email +
+для модели Post обязательными являются user_id, title, body +
+создайте связи relationship между моделями: User.posts и Post.user +
+"""
+
+from sqlalchemy.orm import (
+    declarative_base,
+    sessionmaker,
+    relationship
+)
+from sqlalchemy import (
+    Column,
+    String,
+    Integer,
+    ForeignKey,
+)
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    create_async_engine,
+)
+import os
+
+DB_ECHO = False
+PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:Doctor8897@localhost:5432/postgres"
+engine: AsyncEngine = create_async_engine(url=PG_CONN_URI, echo=DB_ECHO)
+Base = declarative_base(bind=engine)
+Session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class User(Base):
+    __tablename__ = "hw4_users"
+    json_id = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=True, nullable=False)
+    username = Column(String(100), unique=False, nullable=False)
+    email = Column(String, unique=False, nullable=False)
+    posts = relationship("Post", back_populates="user", uselist=False)
+
+    def __str__(self):
+        return f"User(id={self.id}, username={self.username!r})"
+
+
+class Post(Base):
+    __tablename__ = "hw4_posts"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("hw4_users.id"), nullable=False, unique=False)
+    title = Column(String(100), unique=False, nullable=False, default="")
+    body = Column(String(999), unique=False, nullable=False, default="")
+    user = relationship("User", back_populates="posts", uselist=False)
+
+    def __str__(self):
+        return f"Post(id={self.id}, user_id={self.user_id!r}, title={self.title!r})"
+
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
