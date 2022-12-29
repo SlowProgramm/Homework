7938be39c84ce0fe0a398a8816d7a2664369110c
SlowProgramm <arturzlo13@gmail.com>
2022-12-29 16:58:08 +0300

    Add homework 3 to main branch

---
+++ homework_03/main.py
@@ -0,0 +1,14 @@
+"""
+To start the container, use:
+docker run -it -p 8000:8000 app
+"""
+
+from fastapi import FastAPI
+from view import router as view_router
+
+app = FastAPI()
+app.include_router(view_router)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Otus!"}
